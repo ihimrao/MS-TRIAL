b08d0f08d5404a04469aec62c719738e130893ff
imsarkar <himanshu.11814417@lpu.in>
2024-02-15 10:05:00 +0530

    Added logger routes with handlers

--- Logger/cmd/api/main.go
+++ Logger/cmd/api/main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"mstrail/data"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Config struct {
+	Models data.Models
 }
 
 const (
-	port = "8080"
+	port     = "8080"
+	mongoURL = "mongodb+srv://ihimrao:sciJz7anvmj2vDWB@lms.e5ahgmo.mongodb.net/?retryWrites=true&w=majority"
 )
 
 func main() {
-	app := &Config{}
+	mongoClient, err := ConnectToDB()
+	if err != nil {
+		log.Fatal("Error connecting to Mongo", err)
+	}
+	app := &Config{
+		Models: data.New(mongoClient),
+	}
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
 		Handler: app.Routes(),
 	}
 
-	log.Println("Starting logger service")
-	err := server.ListenAndServe()
+	log.Printf("Starting Logger Service on PORT %s\n", port)
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error listening and serving", err)
 	}
+}
+
+func ConnectToDB() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Error connecting database", err)
+		return nil, err
+	}
+	fmt.Println("Connected to database")
+	return c, nil
 }
--- Logger/data/models.go
+++ Logger/data/models.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,7 +15,7 @@ type Models struct {
 }
 
 type LogEntry struct {
-	Id        string    `json:"id,omitempty" bson:"_id,omitempty"`
+	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
 	Name      string    `json:"name,omitempty" bson:"Name,omitempty"`
 	Data      string    `json:"data,omitempty" bson:"data,omitempty"`
 	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
@@ -29,7 +30,6 @@ func New(mongo *mongo.Client) Models {
 	return Models{
 		LogEntry: LogEntry{},
 	}
-
 }
 
 func (l *LogEntry) Insert(entry LogEntry) error {
@@ -46,8 +46,27 @@ func (l *LogEntry) Insert(entry LogEntry) error {
 	return nil
 }
 
+func (l *LogEntry) Get(id string) (*LogEntry, error) {
+	collection := client.Database("logs").Collection("logs")
+	ID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Panic(err)
+		return nil, err
+	}
+	var entry LogEntry
+	err = collection.FindOne(context.TODO(), bson.M{"_id": ID}).Decode(&entry)
+	if err != nil {
+		log.Panic(err)
+		return nil, err
+	}
+	return &entry, nil
+}
+
 func (l *LogEntry) Delete(id string) error {
 	ID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
 	collection := client.Database("logs").Collection("logs")
 	_, err = collection.DeleteOne(context.Background(), bson.M{"_id": ID})
 	if err != nil {
@@ -56,16 +75,34 @@ func (l *LogEntry) Delete(id string) error {
 	return nil
 }
 
-func (l *LogEntry) Edit(entry LogEntry) error {
-	ID, err := primitive.ObjectIDFromHex(entry.Id)
+func (l *LogEntry) Update(entry LogEntry) (*mongo.UpdateResult, error) {
+	ID, err := primitive.ObjectIDFromHex(entry.ID)
+	if err != nil {
+		return nil, err
+	}
 	collection := client.Database("logs").Collection("logs")
-	_, err = collection.UpdateOne(context.TODO(), bson.M{"_id": ID}, LogEntry{
-		Name:      entry.Name,
-		Data:      entry.Data,
-		UpdatedAt: time.Now(),
+	updatedResult, err := collection.UpdateOne(context.TODO(), bson.M{"_id": ID}, bson.M{
+		"$set": bson.M{
+			"name":       l.Name,
+			"data":       l.Data,
+			"updated_at": time.Now(),
+		},
 	})
 	if err != nil {
+		return nil, err
+	}
+	return updatedResult, err
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != nil {
 		return err
 	}
+
 	return nil
 }
